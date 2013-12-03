5252879d45d9da58687033e32e290a2af7457a40
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2013-12-02 16:41:25 -0800

    Add basicplus.js

---
+++ vendor/assets/javascripts/jquery-fileupload/basicplus.js
@@ -0,0 +1,5 @@
+//=require jquery-fileupload/vendor/load-image.min
+//=require jquery-fileupload/basic
+//=require jquery-fileupload/jquery.fileupload-process
+//=require jquery-fileupload/jquery.fileupload-image
+//=require jquery-fileupload/jquery.fileupload-validate
